a1f168401d038e4e69c8cb2635e6cda66e654e8d
Justin Bryant <username@users.noreply.github.com'user.namegit config --global user.namegit config --global user.namegit config --global user.email username@users.noreply.github.comuser.namegit config --global user.namegit config --global user.namegit config --global user.email "username@users.noreply.github.com>
2016-09-18 08:17:07 -0600

    created a cardService, angular.js still giving err

--- js/Directives/cardDirectives/cardHeaderDirective.js
+++ js/Directives/cardDirectives/cardHeaderDirective.js
@@ -1,5 +1,6 @@
 'use strict';
 app.directive('cardsApp', function() {
+  // here we will build the logic for providing the name and age of our character card, as located in the header of the card
   return {};
 
 });
---
+++ js/Services/cardService.js
@@ -0,0 +1,4 @@
+app.service('cardService', function($http, $q){
+    //logic needed to provide the fundementals of the card
+
+})
